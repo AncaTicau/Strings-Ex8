f57b146b973e221f0de77d40fa9fb228046a5fef
MariusA <anca.ticau@gmail.com>
2019-06-06 23:11:48 +0300

    Exercise 8

---
+++ Strings-Ex8/Strings-Ex8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Strings_Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string infoEmily = SubsCharactersSet("Ana are mere si pere.", "ere");
+
+            Console.WriteLine(infoEmily);
+
+            Console.ReadKey();
+
+        }
+
+        //Write a method that strips a set of characters from a string.
+        public static string SubsCharactersSet(string input, string charactersToRemove)
+        {
+            string result = input.Replace(charactersToRemove, "");
+
+            return result;
+        }
+
+    }
+}
